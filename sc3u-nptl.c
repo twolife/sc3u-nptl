e2c48565fe1955c95271592c24562e3919ca9505
Sébastien Noel <sebastien@twolife.be>
2024-05-29 12:29:00 +0200

    Move stuffs around, init variables once and for all

--- sc3u-nptl.c
+++ sc3u-nptl.c
@@ -28,7 +28,7 @@
  *  2. POSIX dictate that delivering pthread_kill() with SIGSTOP to a thread
  *     will actually stop _all_ threads in that process.
  *     But with LinuxThreads you could stop/resume a particular thread with
- *     SIGCSTOP/SIGCONT. The game use this as a way of doing synchronisation
+ *     SIGSTOP/SIGCONT. The game use this as a way of doing synchronisation
  *     between threads.
  *
  * The following code is an attempt at fixing those 2 issues and allows you
@@ -46,6 +46,8 @@
 
 int WantDebugLogging;
 int (*real_pthread_kill)(pthread_t, int);
+sigset_t sigset_usr1;
+sigset_t sigset_usr2;
 
 void sig_usr_handler(int sig) {
     if (WantDebugLogging)
@@ -53,19 +55,13 @@ void sig_usr_handler(int sig) {
 
     if (sig == SIGUSR1) {
         int sig_ret;
-        sigset_t sigset1;
-        sigset_t sigset2;
-        sigemptyset(&sigset1);
-        sigemptyset(&sigset2);
-        sigaddset(&sigset1, SIGUSR1);
-        sigaddset(&sigset2, SIGUSR2);
-        pthread_sigmask(SIG_BLOCK, &sigset1, NULL);
+        pthread_sigmask(SIG_BLOCK, &sigset_usr1, NULL);
         if (WantDebugLogging)
             fprintf(stderr, "thread[%d] blocking, waiting for SIGUSR2...\n", gettid());
-        sigwait (&sigset2, &sig_ret);
+        sigwait (&sigset_usr2, &sig_ret);
+        pthread_sigmask(SIG_UNBLOCK, &sigset_usr1, NULL);
         if (WantDebugLogging)
             fprintf(stderr, "thread[%d] SIGUSR2 recieved, unblocking thread\n", gettid());
-        pthread_sigmask(SIG_UNBLOCK, &sigset1, NULL);
     }
 }
 
@@ -79,6 +75,10 @@ void __attribute__((constructor)) modernlinuxsetup() {
         fprintf(stderr, "setting up SIGUSR1 & SIGUSR2 handler\n");
     }
     real_pthread_kill = dlsym(RTLD_NEXT, "pthread_kill");
+    sigemptyset(&sigset_usr1);
+    sigemptyset(&sigset_usr2);
+    sigaddset(&sigset_usr1, SIGUSR1);
+    sigaddset(&sigset_usr2, SIGUSR2);
     signal(SIGUSR1, sig_usr_handler);
     signal(SIGUSR2, sig_usr_handler);
 }
